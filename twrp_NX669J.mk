691fe6632d18bbdc52e433a80b310e5525cda6bd
piashmsubd <gooogleabal@gmail.com>
2024-12-17 01:35:32 +0300

    Update twrp_NX669J.mk

--- twrp_NX669J.mk
+++ twrp_NX669J.mk
@@ -15,12 +15,16 @@ $(call inherit-product, vendor/twrp/config/common.mk)
 # Inherit from NX669J device
 $(call inherit-product, device/nubia/NX669J/device.mk)
 
+# Additional system properties
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.twrp.device=NX669J
+
+# Override build properties
 PRODUCT_BUILD_PROP_OVERRIDES += \
     ro.build.date.utc=$(shell date +%s) \
     ro.build.date=$(shell date)
 
+# Device properties
 PRODUCT_DEVICE := NX669J
 PRODUCT_NAME := twrp_NX669J
 PRODUCT_BRAND := nubia
@@ -32,4 +36,12 @@ PRODUCT_GMS_CLIENTID_BASE := android-zte
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="NX669J-UN-user 11 RKQ1.210614.002 nubia.20210924.043249 release-keys"
 
-BUILD_FINGERPRINT := nubia/NX669J-UN/NX669J-UN:11/RKQ1.210614.002/nubia.20210924.043249:user/release-keys
+BUILD_FINGERPRINT := nubia/NX669J-UN/NX669J:11/RKQ1.210614.002/nubia.20210924.043249:user/release-keys
+
+# Additional A/B configuration
+AB_OTA_UPDATE_SYSTEM := true
+AB_OTA_UPDATE_VENDOR := true
+
+# Additional packages
+PRODUCT_PACKAGES += \
+    some_extra_package_name
